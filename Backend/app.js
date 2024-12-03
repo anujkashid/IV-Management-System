5acc16d0bbf8a0a8d9be29708b54eae0aed4b0c7
anujkashid <anujkashid123@gmail.com>
2024-12-03 14:11:48 +0530

    Second addition

---
+++ Backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const route = require('./route');
+
+
+//  CORS
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/KnightOne")
+
+// Middleware to  JSON 
+app.use(express.json());
+
+// server upload images
+app.use('/images',express.static('Images'))
+
+// Routes
+app.use('/', route);
+
+
+app.get('/', (req, res) => {
+  res.send("Connected to local host.");
+});
+
+// Start the server
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
